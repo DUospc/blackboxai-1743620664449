c232d918abd18f301427b12fc1785589659239d4
DUospc <de.besher123@gmail.com>
2025-04-02 21:16:09 +0200

    Add BrowserApp/MainWindow.xaml.cs 1743621366181

--- BrowserApp/MainWindow.xaml.cs
+++ BrowserApp/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
-using System;
 using System.Windows;
-using System.Windows.Controls;
-using Microsoft.Toolkit.Wpf.UI.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace BrowserApp
 {
@@ -10,85 +9,43 @@ namespace BrowserApp
         public MainWindow()
         {
             InitializeComponent();
-            webView.NavigationStarting += WebView_NavigationStarting;
-            webView.NavigationCompleted += WebView_NavigationCompleted;
-        }
-
-        private void WebView_NavigationStarting(object sender, WebViewNavigationStartingEventArgs e)
-        {
-            txtAddress.Text = e.Uri?.ToString() ?? "about:blank";
-        }
-
-        private void WebView_NavigationCompleted(object sender, WebViewNavigationCompletedEventArgs e)
-        {
-            if (!e.IsSuccess)
-            {
-                MessageBox.Show($"Failed to load {e.Uri}", "Navigation Error", 
-                    MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
-
-        private void btnBack_Click(object sender, RoutedEventArgs e)
-        {
-            if (webView.CanGoBack)
-                webView.GoBack();
-        }
-
-        private void btnForward_Click(object sender, RoutedEventArgs e)
-        {
-            if (webView.CanGoForward)
-                webView.GoForward();
-        }
-
-        private void btnRefresh_Click(object sender, RoutedEventArgs e)
-        {
-            webView.Refresh();
-        }
-
-        private void btnGo_Click(object sender, RoutedEventArgs e)
-        {
-            NavigateToAddress();
-        }
-
-        private void txtAddress_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            if (e.Key == System.Windows.Input.Key.Enter)
-            {
-                NavigateToAddress();
-            }
-        }
-
-        private void NavigateToAddress()
-        {
-            var address = txtAddress.Text;
-            if (!address.StartsWith("http://") && !address.StartsWith("https://"))
-            {
-                address = "https://" + address;
-            }
-            try
-            {
-                webView.Source = new Uri(address);
-            }
-            catch (UriFormatException)
-            {
-                MessageBox.Show("Invalid URL format", "Error", 
-                    MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
-
-        private void btnNewTab_Click(object sender, RoutedEventArgs e)
-        {
-            var newTab = new TabItem();
-            newTab.Header = "New Tab";
+            this.MouseLeftButtonDown += (s, e) => DragMove();
             
-            var newWebView = new WebView();
-            newWebView.Source = new Uri("https://www.google.com");
-            newWebView.NavigationStarting += WebView_NavigationStarting;
-            newWebView.NavigationCompleted += WebView_NavigationCompleted;
-            
-            newTab.Content = newWebView;
-            tabControl.Items.Add(newTab);
-            tabControl.SelectedItem = newTab;
+            // Initialize settings
+            if (TransparencySlider != null)
+            {
+                TransparencySlider.ValueChanged += (s, e) => 
+                    this.Opacity = TransparencySlider.Value;
+            }
+        }
+
+        private void MinimizeWindow(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeWindow(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = this.WindowState == WindowState.Maximized 
+                ? WindowState.Normal 
+                : WindowState.Maximized;
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void OpenSettings(object sender, RoutedEventArgs e)
+        {
+            if (SettingsPanel != null)
+                SettingsPanel.Visibility = Visibility.Visible;
+        }
+
+        private void CloseSettings(object sender, RoutedEventArgs e)
+        {
+            if (SettingsPanel != null)
+                SettingsPanel.Visibility = Visibility.Collapsed;
         }
     }
 }
