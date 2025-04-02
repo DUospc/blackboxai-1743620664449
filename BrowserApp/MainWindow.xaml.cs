07998e2781b4ed8f33ffa8c89de5759b3dd54c61
DUospc <de.besher123@gmail.com>
2025-04-02 21:21:05 +0200

    Add BrowserApp/MainWindow.xaml.cs 1743621661659

--- BrowserApp/MainWindow.xaml.cs
+++ BrowserApp/MainWindow.xaml.cs
@@ -36,6 +36,21 @@ namespace BrowserApp
             this.Close();
         }
 
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                EditorTextBox.Text = System.IO.File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void OpenEdit(object sender, RoutedEventArgs e)
+        {
+            // Implement edit operations here
+            MessageBox.Show("Edit features coming soon!", "Edit Menu", MessageBoxButton.OK);
+        }
+
         private void OpenSettings(object sender, RoutedEventArgs e)
         {
             if (SettingsPanel != null)
