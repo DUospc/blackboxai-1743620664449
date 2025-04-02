ee093d5485336af34c5d6f21d9d40577bbdfd46f
DUospc <de.besher123@gmail.com>
2025-04-02 21:16:08 +0200

    Add BrowserApp/App.xaml.cs 1743621364715

--- BrowserApp/App.xaml.cs
+++ BrowserApp/App.xaml.cs
@@ -7,6 +7,13 @@ namespace BrowserApp
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+            
+            // Set application-wide resources if needed
+            Current.Resources.MergedDictionaries.Add(
+                new ResourceDictionary
+                {
+                    Source = new System.Uri("pack://application:,,,/MaterialDesignThemes.Wpf;component/Themes/MaterialDesignTheme.Light.xaml")
+                });
         }
     }
 }
